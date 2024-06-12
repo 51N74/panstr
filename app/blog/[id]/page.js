82d9a51f2dfcc3d38bb6291d69c0f6ccbdf553f2
51N74 <j.pattanasintara@gmail.com>
2024-06-12 21:37:34 +0700

    add back button

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import Link from "next/link";
 async function getBlog(id){
     const response = await axios.get(`https://6664041f932baf9032a9ab94.mockapi.io/blog/${id}`)
     return response.data
@@ -12,6 +12,8 @@ export default  async function Page({params}) {
             <h1>Title: {blog.title}</h1>
             <p> Body: {blog.content}</p>
             <p>Author: {blog.name}</p>
+
+            <Link href="/">Back</Link>
         </>
     );
 }
