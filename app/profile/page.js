c8dc5609986265cf401346bcd7cd3797c2eec8c7
51N74 <j.pattanasintara@gmail.com>
2024-06-13 21:24:14 +0700

    add middleware

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,6 +1,6 @@
 import { getSession } from '@auth0/nextjs-auth0';
 
-export default async function ProfileServer() {
+export default async function Profile() {
   const { user } = await getSession();
 
   return (
