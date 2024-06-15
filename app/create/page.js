352c7d56b7b4a4fe1381c267527a5ecab15d80c5
51N74 <j.pattanasintara@gmail.com>
2024-06-15 22:56:49 +0700

    button create page

--- app/create/page.js
+++ app/create/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function Create() {
   return (
@@ -285,9 +286,13 @@ export default function Create() {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
+
+        <Link href="/" className="text-sm font-semibold leading-6 text-gray-900">
         <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
           Cancel
         </button>
+        </Link>
+
         <button
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
