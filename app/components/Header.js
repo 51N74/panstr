652b08b9a0b136e4b3edde79113e2a7a147c19c4
51N74 <j.pattanasintara@gmail.com>
2024-06-15 22:40:43 +0700

    add Loin{user picture}, Logout

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,95 +1,84 @@
-'use client'
-import { useState } from 'react';
-
-
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
 
 export default function Header() {
-    const [isLogin, setIsLogin] = useState(false);
-    
+  const { user } = useUser();
 
-    const showLogin = () => {
-        setIsLogin(!isLogin);
-      };
+  if (!user) {
+    return (
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl" href="/">
+            Panstr
+          </a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
+            />
+          </div>
 
+          <Link href="/api/auth/login">
+          <div>Login</div>
+          </Link>
+         
+        </div>
+      </div>
+    );
+  }
 
-      let greetingMessage;
-      if (isLogin) {
-        greetingMessage = 'Welcome back!';
-      } else {
-        greetingMessage = 'Please log in.';
-      }
-    
   return (
     <>
-    
-    
-
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl" href="/">
-          Panstr
-        </a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl" href="/">
+            Panstr
+          </a>
         </div>
-
-        <div>
-        
-    </div>
-        
-    <div>
-
-    
-      
-    </div>
-
-    <div>
-      <h1>{greetingMessage}</h1>
-      <button onClick={() => setIsLogin(!isLogin)}>
-        {isLogin ? 'Log out' : 'Log in'}
-      </button>
-    </div>
-
-
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
-            </div>
+        <div className="flex-none gap-2">
+          <div className="form-control">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-24 md:w-auto"
+            />
           </div>
-          <ul
-            tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a className="justify-between" href="/profile">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a href="/api/auth/logout">Logout</a>
-            </li>
-          </ul>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                
+                <img src={user.picture} alt={user.name} />
+              </div>
+              
+            </div>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between" href="/profile">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a href="/api/auth/logout">Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
